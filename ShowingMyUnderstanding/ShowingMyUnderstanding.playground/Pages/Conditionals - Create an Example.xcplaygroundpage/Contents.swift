a760b2ac239b175e234cc4c875e6b52352b84235
Student <student@macbook-pro-0863-sebastian-danson.local>
2017-02-24 15:33:54 -0500

    Created example of a conditional

--- ShowingMyUnderstanding/ShowingMyUnderstanding.playground/Pages/Conditionals - Create an Example.xcplaygroundpage/Contents.swift
+++ ShowingMyUnderstanding/ShowingMyUnderstanding.playground/Pages/Conditionals - Create an Example.xcplaygroundpage/Contents.swift
@@ -19,7 +19,27 @@ import PlaygroundSupport
 // Create canvas
 let canvas = Canvas(width: 300, height: 300)
 
-// Replace this command and add your code below...
+// Generate a random number
+var number = random(from: 0, toButNotIncluding: 3)
+
+// when the random number 1 is generated
+if number == 1 {
+    canvas.drawShapesWithFill = true
+    canvas.fillColor = Color.red
+    canvas.drawRectangle(bottomLeftX: 100, bottomLeftY: 100, width: 100, height: 100)
+    
+}
+
+// when the random number 0 is generated
+if number == 0 {
+    canvas.fillColor = Color.blue
+    canvas.drawEllipse(centreX: 150, centreY: 150, width: 100, height: 100, borderWidth: 0)
+}
+
+// when the random number 2 is generated
+if number == 2 {
+    print("Seb is awesome")
+}
 
 /*:
  ## Template code
